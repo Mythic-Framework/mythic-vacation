e8dd13e86b3fbf50d8cf4b58592d4278b719359f
Artmines <arturowells@gmail.com>
2026-04-01 07:01:00 -0230

    I need a vacation - version checkers

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
-
+version '1.0.0'
+repository 'https://github.com/Mythic-Framework/mythic-vacation'
 client_scripts {
     'client/*.lua',
 }
+server_script 'server/version.lua'
